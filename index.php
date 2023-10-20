005c6228e0539a3257c55535005fc14f6be1ac7b
Nicolas Lobos <nicolaslobos495@gmail.com>
2023-10-20 19:02:39 -0300

    *

--- index.php
+++ index.php
@@ -103,13 +103,13 @@ function checklen()
                                     
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <input type="text" class="form-control"  placeholder="First Name *" name="fname"  onkeydown="return alphaOnly(event);" required/>
+                                            <input type="text" class="form-control"  placeholder="Nombre *" name="fname"  onkeydown="return alphaOnly(event);" required/>
                                         </div>
                                         <div class="form-group">
-                                            <input type="email" class="form-control" placeholder="Your Email *" name="email"  />
+                                            <input type="email" class="form-control" placeholder="Email *" name="email"  />
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control" placeholder="Password *" id="password" name="password" onkeyup='check();' required/>
+                                            <input type="password" class="form-control" placeholder="Contraseña *" id="password" name="password" onkeyup='check();' required/>
                                         </div>
                                         
                                         <div class="form-group">
@@ -129,16 +129,16 @@ function checklen()
                                 
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="Last Name *" name="lname" onkeydown="return alphaOnly(event);" required/>
+                                            <input type="text" class="form-control" placeholder="Apellido *" name="lname" onkeydown="return alphaOnly(event);" required/>
                                         </div>
                                         
                                         <div class="form-group">
-                                            <input type="tel" minlength="10" maxlength="10" name="contact" class="form-control" placeholder="Your Phone *"  />
+                                            <input type="tel" minlength="10" maxlength="10" name="contact" class="form-control" placeholder="Telefono *"  />
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control"  id="cpassword" placeholder="Confirm Password *" name="cpassword"  onkeyup='check();' required/><span id='message'></span>
+                                            <input type="password" class="form-control"  id="cpassword" placeholder="Confirmar Contraseña *" name="cpassword"  onkeyup='check();' required/><span id='message'></span>
                                         </div>
-                                        <input type="submit" class="btnRegister" name="patsub1" onclick="return checklen();" value="Register"/>
+                                        <input type="submit" class="btnRegister" name="patsub1" onclick="return checklen();" value="Registrar"/>
                                     </div>
 
                                 </div>
@@ -152,15 +152,15 @@ function checklen()
                                 <div class="row register-form">
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="User Name *" name="username3" onkeydown="return alphaOnly(event);" required/>
+                                            <input type="text" class="form-control" placeholder="Nombre Usuario *" name="username3" onkeydown="return alphaOnly(event);" required/>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <input type="password" class="form-control" placeholder="Password *" name="password3" required/>
+                                            <input type="password" class="form-control" placeholder="Contraseña *" name="password3" required/>
                                         </div>
                                         
-                                        <input type="submit" class="btnRegister" name="docsub1" value="Login"/>
+                                        <input type="submit" class="btnRegister" name="docsub1" value="Ingresar"/>
                                     </div>
                                 </div>
                             </form>
@@ -173,7 +173,7 @@ function checklen()
                                 <div class="row register-form">
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="User Name *" name="username1" onkeydown="return alphaOnly(event);" required/>
+                                            <input type="text" class="form-control" placeholder="Nombre Usuario*" name="username1" onkeydown="return alphaOnly(event);" required/>
                                         </div>
                                         
 
@@ -181,10 +181,10 @@ function checklen()
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <input type="password" class="form-control" placeholder="Password *" name="password2" required/>
+                                            <input type="password" class="form-control" placeholder="Contraseña *" name="password2" required/>
                                         </div>
                                         
-                                        <input type="submit" class="btnRegister" name="adsub" value="Login"/>
+                                        <input type="submit" class="btnRegister" name="adsub" value="Entrar"/>
                                     </div>
                                 </div>
                             </form>
