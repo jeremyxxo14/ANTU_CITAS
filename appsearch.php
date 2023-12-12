9678bcbd1ccc65167974aa22e37e99a95edb4634
Nicolas Lobos <nicolaslobos495@gmail.com>
2023-12-12 01:36:15 -0300

    c

--- appsearch.php
+++ appsearch.php
@@ -79,4 +79,4 @@ if(isset($_POST['app_search_submit']))
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> 
 </body>
-</html>
+</html> 
